032db13594c8c431477f828490767d013e29d9b0
Matheus Cardoso <matheus.mcas@gmail.com>
2016-08-16 19:01:51 -0300

    fix test fpr 3x3_2KINGS_1ROOK scenario

--- tests_challenge.py
+++ tests_challenge.py
@@ -5,10 +5,15 @@ import challenge
 def test_challenge_3x3_2KINGS_1ROOK():
     perm = [chess.KING, chess.KING, chess.ROOK]
     M = N = 3
-    valid_configurations = challenge.chess_challenge(perm, M, N)
-    for uc in valid_configurations:
-        print uc
-    assert True
+    expected_results =[
+        [chess.KING, 0, chess.KING, 0, 0, 0, 0, chess.ROOK, 0],                                                                                                                                                               
+        [chess.KING, 0, 0, 0, 0, chess.ROOK, chess.KING, 0, 0],                                                                                                                                                              
+        [0, 0, chess.KING, chess.ROOK, 0, 0, 0, 0, chess.KING],                                                                                                                                                               
+        [0, chess.ROOK, 0, 0, 0, 0, chess.KING, 0, chess.KING],   
+    ]
+    results = challenge.chess_challenge(perm, M, N)
+    for uc in results:
+        assert (uc in expected_results) == True
 
 def test_challenge_4x4_2ROOKS_4KNIGHTS():
     pass
