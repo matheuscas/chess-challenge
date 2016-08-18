034d1cdb1c98b8a967ffa8516d2c442a75168ab4
Matheus Cardoso <matheus.mcas@gmail.com>
2016-08-18 00:10:59 -0300

    top pylinting

--- util.py
+++ util.py
@@ -1,32 +1,38 @@
-def get_col_by_index(index, N):
+"""
+    This module holds accessory functions that helps to accomplish the other tasks,
+    such as calculates a piece column/line based on index and the number of
+    columns or lines.
+"""
+
+
+def get_col_by_index(index, cols):
     """
     It calculates the chess board column based on array index representations
     """
+    return index % cols
 
-    return index % N
 
-def get_line_by_index(index, M):
+def get_line_by_index(index, lines):
     """
     It calculates the chess board line based on array index representations
     """
-
-    if index < M:
+    if index < lines:
         return 1
-    elif index == M:
+    elif index == lines:
         return 2
-    elif index >= (M - 1) * M:
-        return M
+    elif index >= (lines - 1) * lines:
+        return lines
     else:
-        for l in range(2, M):
-            if index >= (l - 1) * M and index < l * M:
-                return l
+        for line in range(2, lines):
+            if index >= (line - 1) * lines and index < line * lines:
+                return line
     return -1
+
 
 def has_valid_number_pieces(configuration, original_permutation):
     """
     Checks if a certain chess configuration has the valid number of pieces
     """
-
     pieces = set(original_permutation)
     while len(pieces) > 0:
         piece = pieces.pop()
